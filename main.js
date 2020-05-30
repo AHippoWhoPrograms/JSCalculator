0c2f21ec5c24c6a6738158376871e003e0a17fa5
AHippoWhoPrograms <mjduboc@gmail.com>
2020-05-30 17:40:35 -0400

    Finshed implementing regex and conversion to postfix

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const MAX_BOXES = 20;
-
 var keyboard = document.querySelector("#calculator > #components > #keyboard");
 var input = document.querySelector("#calculator > #components > #textbox");
 
@@ -7,7 +6,79 @@ keyboard.style.gridTemplateColumns = "1fr 1fr 1fr 1fr";
 
 function evaluateExpression()
 {
-    var pattern = /[[0-9]+[\+|-|\*|\/]]+/i;
+    const precedence = { "*":2, "/":2, "+":1, "-":1, "(":3};
+    var pattern = /^((\(*([0-9|\)*]+[\+|\-|\*|\/]?)\)*)+)$/;
+    var input_str = input.value;
+    var queue = [];
+    var stack = [];
+    var operands = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var operators = ["*", "+", "-", "/", "(", ")"];
+    var isOperand = true;
+    var isOperator = false;
+    if(pattern.test(input_str) === false) return "Not a valid expression!";
+
+    for(var input_str_itr = 0; input_str_itr < input_str.length; input_str_itr++)
+    {
+        var current = input_str[input_str_itr];
+
+        //Every time we switch from an operand to an operator or vice versa,
+        //we add a space to the queue.
+        if(current in operands)
+        {
+            if(!isOperand)
+            {
+                queue.push(" ");
+                isOperand = true;
+                isOperator = false;
+            }
+            queue.push(current);
+        }
+
+        else if(operators.indexOf(current) !== -1)
+        {
+            if(isOperand)
+            {
+                isOperand = false;
+                isOperator = true;
+            }
+            if(current == "(") stack.push(current);
+            else if(current == ")")
+            {
+                while(stack[stack.length - 1] !== "(")
+                {
+                    if(stack.length === 0) return "Invalid parentheses pairing!";
+                    else 
+                    {
+                        queue.push(" ");
+                        queue.push(stack.pop());
+                    }
+                };
+                stack.pop();
+            }
+        
+            else if(stack.length !== 0 && precedence[current] <= precedence[stack[stack.length - 1]])
+            {
+                while(precedence[current] <= precedence[stack[stack.length - 1]])
+                {
+                    if(precedence[stack[stack.length - 1]] === 3) break;
+                    var top = stack.pop();
+                    queue.push(" ");
+                    queue.push(top);
+                }
+                stack.push(current);
+            }
+            else if(stack.length === 0 || precedence[current] > precedence[stack[stack.length - 1]]) stack.push(current);
+        }
+    }
+
+    while(stack.length !== 0)
+    {
+        queue.push(" ");
+        queue.push(stack.pop());
+    }
+    return queue.join("");
+    
+
 }
 
 for(var i = 0; i < MAX_BOXES; i++)
@@ -41,7 +112,7 @@ for(var i = 0; i < MAX_BOXES; i++)
             button.setAttribute("data-key", "56");
             break;
         case 6:
-            button.textContent = "9"
+            button.textContent = "9";
             button.setAttribute("data-key", "57");
             break;
         case 7:
